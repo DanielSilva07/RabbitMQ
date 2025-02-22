b097c64f32338b8ecf5112affa54d41f82b9957f
DanielSilva07 <daniel97silva.ds@gmail.com>
2025-02-22 12:37:44 -0300

    :bricks: ci: Add Listener RabbitMQ for imcApplication

---
+++ src/main/java/com/daniel/silva/RabbitMQ/listener/ImcListener.java
@@ -0,0 +1,24 @@
+package com.daniel.silva.RabbitMQ.listener;
+import com.daniel.silva.RabbitMQ.constante.Mensagem;
+import com.daniel.silva.RabbitMQ.domain.Cliente;
+import com.daniel.silva.RabbitMQ.service.NotificationService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ImcListener {
+
+    private final NotificationService notificationService;
+
+    public ImcListener(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @RabbitListener(queues = "${rabbitmq.queue.imc}")
+    public void listenToImcQueue(Cliente cliente) {
+        String mensagem = String.format(Mensagem.IMC,"O IMC do %s e de %s", cliente.getNome(),
+                cliente.getImc());
+        notificationService.sendNotification(mensagem);
+
+    }
+}
