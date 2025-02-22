ddc006e4f3a7b401eb96f7cbad0fd13e7d02ee3e
DanielSilva07 <daniel97silva.ds@gmail.com>
2025-02-22 12:43:07 -0300

    :bricks: ci: Add RabbitMQ Config

--- src/main/java/com/daniel/silva/RabbitMQ/config/ConfigRabbitMQ.java
+++ src/main/java/com/daniel/silva/RabbitMQ/config/ConfigRabbitMQ.java
@@ -1,41 +1,16 @@
 package com.daniel.silva.RabbitMQ.config;
 
-import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConfigRabbitMQ {
 
-    private ConnectionFactory connectionFactory;
 
     @Bean
-    public Queue criarFilaAgendamento() {
-        return QueueBuilder.durable("agendamento-feito.ms-agendamento").build();
-    }
-
-    @Bean
-    public RabbitAdmin CriarRabbitAdmin(ConnectionFactory connectionFactory) {
-        return new RabbitAdmin(connectionFactory);
-    }
-
-    @Bean
-    public ApplicationListener<ApplicationReadyEvent>inicializarAdmin(RabbitAdmin rabbitAdmin) {
-        return event -> rabbitAdmin.initialize();
-    }
-
-    @Bean
-    public FanoutExchange criarExchangeAgendamento() {
-        return ExchangeBuilder.fanoutExchange("agendamento-exchange").build();
-    }
-
-    @Bean
-    public Binding criarBindingAgendamento() {
-        return BindingBuilder.bind(criarFilaAgendamento()).to(criarExchangeAgendamento());
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 
 
