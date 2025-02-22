a1866e605023a177ab959dcf6f208cebf0fc9ef6
DanielSilva07 <daniel97silva.ds@gmail.com>
2025-02-22 12:39:28 -0300

    :bricks: ci: Add Message constante

---
+++ src/main/java/com/daniel/silva/RabbitMQ/constante/Mensagem.java
@@ -0,0 +1,11 @@
+package com.daniel.silva.RabbitMQ.constante;
+
+public class Mensagem {
+
+    public static final String IMC = " O IMC do  %s e de %s" ;
+
+    public void  IMC(){
+      String imc = Mensagem.IMC;
+    }
+
+}
