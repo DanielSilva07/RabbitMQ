029453684c5dd5e9ac73389ca689f450c194ae5d
DanielSilva07 <daniel97silva.ds@gmail.com>
2025-02-23 15:35:14 -0300

    :bricks: ci: Tratamento de exception

--- src/main/java/com/daniel/silva/RabbitMQ/config/ConfigRabbitMQ.java
+++ src/main/java/com/daniel/silva/RabbitMQ/config/ConfigRabbitMQ.java
@@ -1,5 +1,6 @@
 package com.daniel.silva.RabbitMQ.config;
 
+import com.daniel.silva.RabbitMQ.exceptions.TratamentoErrorHandler;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
