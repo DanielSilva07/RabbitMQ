7e83df3ce6bf75447d54f868c545e92f16e73ffe
DanielSilva07 <daniel97silva.ds@gmail.com>
2025-02-23 15:40:41 -0300

    fix black space

--- src/main/java/com/daniel/silva/RabbitMQ/constante/Mensagem.java
+++ src/main/java/com/daniel/silva/RabbitMQ/constante/Mensagem.java
@@ -2,7 +2,7 @@ package com.daniel.silva.RabbitMQ.constante;
 
 public class Mensagem {
 
-    public static final String IMC = " O IMC do  %s e de %s" ;
+    public static final String IMC = " O IMC do %s e de %s" ;
 
     public void  IMC(){
       String imc = Mensagem.IMC;
